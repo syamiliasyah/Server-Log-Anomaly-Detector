8dd65f2b8c4c288b5be87804901c1ad362099404
Syamilia S. <syamiliasyah@gmail.com>
2026-09-23 11:01:48 +0800

    Create mock_logs_generator.py

---
+++ mock_logs_generator.py
@@ -0,0 +1,11 @@
+# Simulated log file for demonstration & QA testing
+
+SAMPLE_LOGS = """
+2026-09-23 10:00:01 192.168.1.100 Failed password for invalid user admin from 192.168.1.100 port 45222 ssh2
+2026-09-23 10:00:03 192.168.1.100 Failed password for invalid user root from 192.168.1.100 port 45224 ssh2
+2026-09-23 10:00:05 192.168.1.100 Failed password for invalid user test from 192.168.1.100 port 45226 ssh2
+2026-09-23 10:00:07 192.168.1.100 Failed password for invalid user admin from 192.168.1.100 port 45228 ssh2
+2026-09-23 10:01:15 10.0.0.50 GET /.env HTTP/1.1 404
+2026-09-23 10:01:18 10.0.0.50 GET /admin/config.php HTTP/1.1 404
+2026-09-23 10:02:00 192.168.1.50 Accepted password for user syamilia from 192.168.1.50 port 51100 ssh2
+"""
